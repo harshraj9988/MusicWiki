376071bb919736d1b7ff2ebb99601ae05c916152
harshraj9988 <harshraj9988@gmail.com>
2023-01-21 09:43:37 +0530

    modified artist info genre chip

--- app/src/main/java/com/example/musicwiki/presentation/ui/album_info_screen/AlbumInfoFragment.kt
+++ app/src/main/java/com/example/musicwiki/presentation/ui/album_info_screen/AlbumInfoFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.musicwiki.R
 import com.example.musicwiki.databinding.FragmentAlbumInfoBinding
@@ -38,7 +39,9 @@ class AlbumInfoFragment : Fragment() {
 
         val adapter = ArtistGenresListAdapter(ArtistGenresClickListener {
             it?.let {
-
+                Navigation.findNavController(requireView()).navigate(
+                    AlbumInfoFragmentDirections.actionAlbumInfoFragmentToGenreInfoFragment(it.name)
+                )
             }
         })
 
